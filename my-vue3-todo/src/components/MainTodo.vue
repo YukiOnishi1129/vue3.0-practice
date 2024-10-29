1efc0180c168a170cea44b48dfe9a52b99a15230
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-10-29 23:00:07 +0900

    v-show

--- my-vue3-todo/src/components/MainTodo.vue
+++ my-vue3-todo/src/components/MainTodo.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue'
 // inputの値と連動するリアクティブ関数を作成
 const todo = ref('')
 const todoList = ref<{ id: number; task: string }[]>([])
+const isEdit = ref(false)
 
 const ls = localStorage.todoList
 
@@ -18,12 +19,24 @@ const addTodo = () => {
 
   todo.value = ''
 }
+
+const showTodo = (id: number) => {
+  const findTodo = todoList.value.find((todo) => todo.id === id)
+
+  if (findTodo) {
+    todo.value = findTodo.task
+    isEdit.value = true
+  }
+}
+
+const editTodo = () => {}
 </script>
 
 <template>
   <div>
     <input type="text" class="todo_input" v-model="todo" placeholder="+ TODOを入力" />
-    <button class="btn" @click="addTodo">追加</button>
+    <button class="btn green" @click="editTodo" v-show="isEdit">変更</button>
+    <button class="btn" @click="addTodo" v-show="!isEdit">追加</button>
   </div>
 
   <div class="box_list">
@@ -33,7 +46,7 @@ const addTodo = () => {
         <label>{{ todo.task }}</label>
       </div>
       <div class="btns">
-        <button class="btn green">編</button>
+        <button class="btn green" @click="showTodo(todo.id)">編</button>
         <button class="btn pink">削</button>
       </div>
     </div>
