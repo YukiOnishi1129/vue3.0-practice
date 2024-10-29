dd4fd2fef245b9d8e563d728663cefecbe549f4f
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-10-29 22:38:22 +0900

    v-model

--- my-vue3-todo/src/components/MainTodo.vue
+++ my-vue3-todo/src/components/MainTodo.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// inputの値と連動するリアクティブ関数を作成
+const todo = ref('')
+
+const addTodo = () => {
+  console.log(todo.value)
+}
+</script>
 
 <template>
   <div>
-    <input type="text" class="todo_input" placeholder="+ TODOを入力" />
-    <button class="" btn>追加</button>
+    <input type="text" class="todo_input" v-model="todo" placeholder="+ TODOを入力" />
+    <button class="btn" @click="addTodo">追加</button>
   </div>
 </template>
 
