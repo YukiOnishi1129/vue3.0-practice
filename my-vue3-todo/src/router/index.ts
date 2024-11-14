231147d8d0ae2281fd4863d680c0280972aa0b23
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-11-14 18:35:38 +0900

    add page

--- my-vue3-todo/src/router/index.ts
+++ my-vue3-todo/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import AboutPage from '@/views/AboutPage.vue'
 import MainTodo from '@/views/MainTodo.vue'
 
 const router = createRouter({
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/mainTodo',
       name: 'mainTodo',
       component: MainTodo,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutPage,
     },
   ],
 })
